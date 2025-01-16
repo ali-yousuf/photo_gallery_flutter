adf056c10b49c64f656a5a70944d7be97b3b3be4
md-ali-yousuf <yousuf.vivasoft@bKash.com>
2025-01-16 15:50:06 +0600

    Write album and photo list cubit test

---
+++ test/presentation/albums/albums_cubit_test.dart
@@ -0,0 +1,83 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:photo_gallery/domain/entities/album_entity.dart';
+import 'package:photo_gallery/domain/repository/album_repository.dart';
+import 'package:photo_gallery/presentation/albums/cubits/albums_cubit.dart';
+
+import 'albums_cubit_test.mocks.dart';
+
+@GenerateMocks([AlbumRepository])
+void main() {
+  late MockAlbumRepository mockAlbumRepository;
+  late AlbumsCubit albumsCubit;
+
+  final List<AlbumEntity> mockAlbums = [
+    AlbumEntity(name: 'Vacation', thumbnail: 'vacation_thumb.jpg', photoCount: 50),
+    AlbumEntity(name: 'Birthday', thumbnail: 'birthday_thumb.jpg', photoCount: 30),
+  ];
+
+  setUp(() {
+    mockAlbumRepository = MockAlbumRepository();
+    albumsCubit = AlbumsCubit(mockAlbumRepository);
+  });
+
+  tearDown(() {
+    albumsCubit.close();
+  });
+
+  group('AlbumsCubit', () {
+    test('initial state should be AlbumsInitial', () {
+      expect(albumsCubit.state, AlbumsInitial());
+    });
+
+    blocTest<AlbumsCubit, AlbumsState>(
+      'emits [AlbumsLoading, AlbumsLoaded] when loadAlbums succeeds and albums are found',
+      build: () => albumsCubit,
+      setUp: () {
+        when(mockAlbumRepository.getAlbums()).thenAnswer((_) async => mockAlbums);
+      },
+      act: (cubit) => cubit.loadAlbums(),
+      expect: () => [
+        AlbumsLoading(),
+        AlbumsLoaded(albums: mockAlbums),
+      ],
+      verify: (cubit) {
+        verify(mockAlbumRepository.getAlbums()).called(1);
+      },
+    );
+
+    blocTest<AlbumsCubit, AlbumsState>(
+      'emits [AlbumsLoading, AlbumNotFound] when loadAlbums succeeds but no albums are found',
+      build: () => albumsCubit,
+      setUp: () {
+        when(mockAlbumRepository.getAlbums()).thenAnswer((_) async => []);
+      },
+      act: (cubit) => cubit.loadAlbums(),
+      expect: () => [
+        AlbumsLoading(),
+        AlbumNotFound(),
+      ],
+      verify: (cubit) {
+        verify(mockAlbumRepository.getAlbums()).called(1);
+      },
+    );
+
+    blocTest<AlbumsCubit, AlbumsState>(
+      'emits [AlbumsLoading, AlbumsFailure] when loadAlbums fails',
+      build: () => albumsCubit,
+      setUp: () {
+        when(mockAlbumRepository.getAlbums()).thenThrow(Exception('Error fetching albums'));
+      },
+      act: (cubit) => cubit.loadAlbums(),
+      expect: () => [
+        AlbumsLoading(),
+        AlbumsFailure(),
+      ],
+      verify: (cubit) {
+        verify(mockAlbumRepository.getAlbums()).called(1);
+      },
+    );
+  });
+}
---
+++ test/presentation/photos/photo_list_cubit_test.dart
@@ -0,0 +1,83 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:photo_gallery/domain/entities/photo_entity.dart';
+import 'package:photo_gallery/domain/repository/photo_list_repository.dart';
+import 'package:photo_gallery/presentation/photos/cubits/photo_list_cubit.dart';
+
+import 'photo_list_cubit_test.mocks.dart';
+
+@GenerateMocks([PhotoListRepository])
+void main() {
+  late MockPhotoListRepository mockPhotoListRepository;
+  late PhotoListCubit photoListCubit;
+
+  final List<PhotoEntity> mockPhotos = [
+    PhotoEntity(path: 'photo1.jpg'),
+    PhotoEntity(path: 'photo2.jpg'),
+  ];
+
+  setUp(() {
+    mockPhotoListRepository = MockPhotoListRepository();
+    photoListCubit = PhotoListCubit(mockPhotoListRepository);
+  });
+
+  tearDown(() {
+    photoListCubit.close();
+  });
+
+  group('PhotoListCubit', () {
+    test('initial state should be PhotoListInitial', () {
+      expect(photoListCubit.state, PhotoListInitial());
+    });
+
+    blocTest<PhotoListCubit, PhotoListState>(
+      'emits [PhotoListLoading, PhotoListLoaded] when loadPhotoList succeeds and photos are found',
+      build: () => photoListCubit,
+      setUp: () {
+        when(mockPhotoListRepository.getPhotoList(any)).thenAnswer((_) async => mockPhotos);
+      },
+      act: (cubit) => cubit.loadPhotoList('album1'),
+      expect: () => [
+        PhotoListLoading(),
+        PhotoListLoaded(photoList: mockPhotos),
+      ],
+      verify: (cubit) {
+        verify(mockPhotoListRepository.getPhotoList('album1')).called(1);
+      },
+    );
+
+    blocTest<PhotoListCubit, PhotoListState>(
+      'emits [PhotoListLoading, NoPhotoFound] when loadPhotoList succeeds but no photos are found',
+      build: () => photoListCubit,
+      setUp: () {
+        when(mockPhotoListRepository.getPhotoList(any)).thenAnswer((_) async => []);
+      },
+      act: (cubit) => cubit.loadPhotoList('album1'),
+      expect: () => [
+        PhotoListLoading(),
+        NoPhotoFound(),
+      ],
+      verify: (cubit) {
+        verify(mockPhotoListRepository.getPhotoList('album1')).called(1);
+      },
+    );
+
+    blocTest<PhotoListCubit, PhotoListState>(
+      'emits [PhotoListLoading, PhotoListFailure] when loadPhotoList fails',
+      build: () => photoListCubit,
+      setUp: () {
+        when(mockPhotoListRepository.getPhotoList(any)).thenThrow(Exception('Error fetching photos'));
+      },
+      act: (cubit) => cubit.loadPhotoList('album1'),
+      expect: () => [
+        PhotoListLoading(),
+        PhotoListFailure(),
+      ],
+      verify: (cubit) {
+        verify(mockPhotoListRepository.getPhotoList('album1')).called(1);
+      },
+    );
+  });
+}
